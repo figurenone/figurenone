cc6844983cbc02dd9b94616df4790b710887b93e
Reid Bright <air24@Reids-MacBook-Air.local>
2025-06-30 16:20:41 -0400

    Configure Eleventy to passthrough admin folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/admin");
+
   return {
     dir: {
-      input: 'src',
-      output: '_site'
+      input: "src",
+      output: "_site"
     }
   };
 };
