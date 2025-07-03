ff3de7f179a11125599ff3a46d326deae56b4fcf
Reid Bright <air24@Reids-Air.lan>
2025-07-02 22:39:36 -0400

    Fix CMS loading error

--- static/admin/cms.js
+++
@@ -1,8 +0,0 @@
-// src/admin/cms.js
-
-if (window.CMS) {
-  console.log("✅ CMS loaded, initializing...");
-  window.CMS.init();
-} else {
-  console.error("❌ CMS not found on window. Make sure netlify-cms.js is loaded first.");
-}
